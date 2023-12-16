e7625b444950d9b88faed36ec1a106a0498c105b
aimee rivers <aimee@aimeerivers.com>
2023-12-16 19:24:42 +0100

    chore(eslint): use this project to lint itself

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
 module.exports = {
   root: true,
   extends: [
-    "standard-with-typescript",
-    "plugin:prettier/recommended",
-    "plugin:@typescript-eslint/recommended"
+    'standard-with-typescript',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
   rules: {
-    "prefer-const": [
-      "error",
+    'prefer-const': [
+      'error',
       {
-        "destructuring": "any",
-        "ignoreReadBeforeAssign": false
-      }
+        destructuring: 'any',
+        ignoreReadBeforeAssign: false,
+      },
     ],
-    "unused-imports/no-unused-imports": "error",
-    "@typescript-eslint/consistent-type-imports": [
-      "error",
+    'unused-imports/no-unused-imports': 'error',
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
       {
-        "prefer": "type-imports",
-        "disallowTypeAnnotations": false
-      }
+        prefer: 'type-imports',
+        disallowTypeAnnotations: false,
+      },
     ],
-    "import/order": [
-      "error",
+    'import/order': [
+      'error',
       {
-        "alphabetize": {
-          "order": "asc",
-          "caseInsensitive": true
-        }
-      }
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
     ],
-    "sort-imports": ["off"],
-    "simple-import-sort/imports": "error"
+    'sort-imports': ['off'],
+    'simple-import-sort/imports': 'error',
   },
-  plugins: ["simple-import-sort", "unused-imports"],
+  plugins: ['simple-import-sort', 'unused-imports'],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module"
-  }
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
 };
