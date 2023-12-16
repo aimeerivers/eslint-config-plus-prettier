4c3d3cc375f26e249271a412452dad32c9d96d1e
aimee rivers <aimee@aimeerivers.com>
2023-12-16 20:39:12 +0100

    feat(index): remove floating promises check

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
   ],
   rules: {
+    '@typescript-eslint/no-floating-promises': ['off'],
     'prefer-const': [
       'error',
       {
