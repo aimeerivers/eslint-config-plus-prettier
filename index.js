31fe630002a81816907500da67af6f13429837e7
aimee rivers <aimee@aimeerivers.com>
2024-05-30 22:21:03 +0200

    feat(deps): remove dependencies (#20)
    * chore(deps): remove eslint and config-love
    
    * chore(deps): udpate eslint-plugin-promise
    
    * chore(deps): remove eslint-plugin-unused-imports

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 module.exports = {
   root: true,
-  extends: ["love", "plugin:prettier/recommended", "plugin:@typescript-eslint/recommended"],
   rules: {
     "@typescript-eslint/no-floating-promises": ["off"],
     "prefer-const": [
@@ -10,27 +9,9 @@ module.exports = {
         ignoreReadBeforeAssign: false,
       },
     ],
-    "unused-imports/no-unused-imports": "error",
-    "@typescript-eslint/consistent-type-imports": [
-      "error",
-      {
-        prefer: "type-imports",
-        disallowTypeAnnotations: false,
-      },
-    ],
-    "import/order": [
-      "error",
-      {
-        alphabetize: {
-          order: "asc",
-          caseInsensitive: true,
-        },
-      },
-    ],
     "sort-imports": ["off"],
     quotes: ["error", "double"],
   },
-  plugins: ["unused-imports"],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
