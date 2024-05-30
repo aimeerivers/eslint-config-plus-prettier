3b75663f0e0efb718bcc9e38b6531dc834045d07
aimee rivers <aimee@aimeerivers.com>
2024-05-30 22:20:05 +0200

    chore(deps): remove eslint-plugin-unused-imports

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 module.exports = {
   root: true,
-  extends: ["plugin:prettier/recommended", "plugin:@typescript-eslint/recommended"],
   rules: {
     "@typescript-eslint/no-floating-promises": ["off"],
     "prefer-const": [
@@ -10,18 +9,9 @@ module.exports = {
         ignoreReadBeforeAssign: false,
       },
     ],
-    "unused-imports/no-unused-imports": "error",
-    "@typescript-eslint/consistent-type-imports": [
-      "error",
-      {
-        prefer: "type-imports",
-        disallowTypeAnnotations: false,
-      },
-    ],
     "sort-imports": ["off"],
     quotes: ["error", "double"],
   },
-  plugins: ["unused-imports"],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
